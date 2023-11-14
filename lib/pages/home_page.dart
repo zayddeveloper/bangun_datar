f6e32df0b9302be8aa43128a3e9fac9a0b5a6ffa
said <msaida97@gmail.com>
2023-11-14 14:40:42 +0700

    custom component

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,29 +8,52 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green.shade300,
-        title: Text(
-          "Home Page",
-          style: TextStyle(color: Colors.white),
-        ),
+        title: CustomText(text: "HomePage",),
       ),
       body: Column(
         children: [
-          Container(
-            margin: EdgeInsets.all(8),
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            decoration: BoxDecoration(
-              color: Colors.green
-            ),
-            child: Text("Persegi"),
+          CustomMenu(imageAssets: "assets/persegi.jpeg",title: "Pesrsegi"),
+          CustomMenu(imageAssets: "assets/segitiga.jpeg",title: "Segitiga"),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomText extends StatelessWidget {
+  const CustomText({
+    super.key, required this.text,
+  });
+final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(color: Colors.white),
+    );
+  }
+}
+
+class CustomMenu extends StatelessWidget {
+  const CustomMenu({
+    super.key, required this.imageAssets, required this.title,
+  });
+  final String imageAssets;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(color: Colors.green),
+      child: Column(
+        children: [
+          Image.asset(
+            imageAssets,
+            height: 100,
           ),
-          Container(
-            margin: EdgeInsets.all(8),
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            decoration: BoxDecoration(
-                color: Colors.green
-            ),
-            child: Text("Segitiga"),
-          )
+          Text(title),
         ],
       ),
     );
